a7976a77e9f63fdbe1dea265a4c2c53002b2adcc
Tanner Kirkpatrick <twk4491@gmail.com>
2021-01-01 13:32:01 -0500

    initial state declared and article reducer built

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
 import Header from "./components/Header";
 import Search from "./components/Search";
+import store from "./store";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Header />
-        <Search />
-      </div>
+      <Provider store={store}>
+        <div className="container">
+          <Header />
+          <Search />
+        </div>
+      </Provider>
     );
   }
 }
---
+++ src/reducers/articleReducer.js
@@ -0,0 +1,18 @@
+import { FETCH_ARTICLES } from "../actions/types";
+
+const initialState = {
+  articles: [],
+  loading: true,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case FETCH_ARTICLES:
+      return {
+        ...state,
+        articles: action.payload,
+      };
+    default:
+      return state;
+  }
+}
