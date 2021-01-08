bdefd578d12eaf16e4f5bf39b7522b9133db6bf2
Tanner Kirkpatrick <twk4491@gmail.com>
2021-01-08 12:29:23 -0500

    removed Spinner.js

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import ArticleItem from "./ArticleItem";
-import Spinner from "./Spinner";
 
 const Articles = ({ article: { articles } }) => {
   if (articles === null) {
